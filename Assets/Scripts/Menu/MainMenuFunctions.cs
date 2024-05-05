d463fa884b8a6050b9dc98da2f29c1c28b57cb2b
MichaelEGA <67586167+MichaelEGA@users.noreply.github.com>
2024-05-05 19:01:58 +0200

    Updated: Fixed mission bug created by removed node

--- Assets/Scripts/Menu/MainMenuFunctions.cs
+++ Assets/Scripts/Menu/MainMenuFunctions.cs
@@ -838,7 +838,7 @@ public static class MainMenuFunctions
     public static void DisplayMessageOnTitleScreen(Text titleScreenMessageBox)
     {
         string[] messages = new string[10];
-        messages[0] = "Welcome to Open Galaxy. Version 2.0.92 brings the new AI tagging feature allowing for more dynamic behaviour.";
+        messages[0] = "Welcome to Open Galaxy. Version 2.2.05 includes updated ai and formation flying.";
         messages[1] = "Open Galaxy's aim is to be a platform for X-Wing and Tie Fighter style custom missions.";
         messages[2] = "All Open Galaxy's missions were created using the inbuilt mission editor.";
         messages[3] = "Flying a ship isn't like dusting crops. Familiarise yourself with the controls first.";

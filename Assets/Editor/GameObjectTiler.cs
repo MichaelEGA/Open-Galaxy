36aa02dd9b0556a30641f4fbbfe63914ea9b94ff
MichaelEGA <67586167+MichaelEGA@users.noreply.github.com>
2025-02-27 16:51:41 +0100

    Added: death star environment

--- Assets/Editor/GameObjectTiler.cs
+++ Assets/Editor/GameObjectTiler.cs
@@ -8,6 +8,7 @@ public class TileGameObjectsEditor : EditorWindow
     private int columns = 5; // Number of columns to tile
     private float spacingX = 1.0f; // Spacing between objects on the X axis
     private float spacingZ = 1.0f; // Spacing between objects on the Y axis
+    private bool randomRotateTiles = false;
 
     [UnityEditor.MenuItem("Tools/Tile GameObjects")]
     public static void ShowWindow()
@@ -41,6 +42,7 @@ public class TileGameObjectsEditor : EditorWindow
         columns = EditorGUILayout.IntField("Columns", columns);
         spacingX = EditorGUILayout.FloatField("Spacing X", spacingX);
         spacingZ = EditorGUILayout.FloatField("Spacing Y", spacingZ);
+        randomRotateTiles = EditorGUILayout.Toggle("Randomly Rotate Tiles", randomRotateTiles);
 
         if (GUILayout.Button("Tile GameObjects"))
         {
@@ -60,10 +62,22 @@ public class TileGameObjectsEditor : EditorWindow
 
         int i = 0;
 
+        int i2 = 0;
+
+        float[] rotation = new float[4];
+        rotation[0] = 0;
+        rotation[1] = 90;
+        rotation[2] = 180;
+        rotation[3] = 270;
+
         for (int z = 0; z < rows; z++)
         {
             for (int x = 0; x < columns; x++)
             {
+                //This randomly selects an object from the list
+                i = Random.Range(0, objectsToTile.Length);
+
+                i2 = Random.Range(0, 4);
 
                 // Calculate the position for the new object
                 Vector3 position = new Vector3(x * spacingX, 0, z * spacingZ);
@@ -71,15 +85,17 @@ public class TileGameObjectsEditor : EditorWindow
                 // Instantiate a new object from the array
                 GameObject obj = GameObject.Instantiate(objectsToTile[i]) as GameObject;
 
+                //This sets teh position
                 obj.transform.position = position;
+
+                //This sets the rotation
+                if (randomRotateTiles == true)
+                {
+                    obj.transform.rotation = Quaternion.Euler(0, rotation[i2], 0);
+                }
 
                 // Set the new object as a child of the parent object
                 obj.transform.parent = parentObject.transform;
-
-                if (i >= objectsToTile.Length)
-                {
-                    i = 0;
-                }
             }
         }
     }
